b0b32d61683e44fc4555c797ee35f6feff8c2781
Guest <guest-qddl1g@h24879.sgb.skir.kr>
2020-12-12 21:17:59 +0900

    mark up and css

---
+++ script.js
@@ -0,0 +1,8 @@
+function enterCard() {
+  document.querySelector(".enterCard").style.display = 'none';
+  document.querySelector(".enters").style.display = 'flex';
+}
+function exitBtn() {
+  document.querySelector(".enterCard").style.display = 'block';
+  document.querySelector(".enters").style.display = 'none';
+}
